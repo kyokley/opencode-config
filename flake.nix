d75c87d4cf77cb9de7c60f686274f4741783160d
Kevin Yokley <kyokley@mars>
2026-03-09 20:31:00 -0500

    Add node2nix

--- flake.nix
+++ flake.nix
@@ -18,36 +18,36 @@
 
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-        devShells.default = pkgs.mkShell {
-          name = "opencode-dev-shell";
-          buildInputs = [
-            pkgs.nodejs
-            inputs.old-nixpkgs.legacyPackages.${system}.node2nix
-            pkgs.bun
-          ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: 
+        let
+          nodePackages = import ./node-packages.nix { inherit pkgs system; };
+        in
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+          devShells.default = pkgs.mkShell {
+            name = "opencode-dev-shell";
+            buildInputs = [
+              pkgs.nodejs
+              inputs.old-nixpkgs.legacyPackages.${system}.node2nix
+              pkgs.bun
+            ];
+          };
+
+          packages.default = nodePackages.package.override {
+            src = ./.;
+            buildInputs = [ pkgs.makeWrapper pkgs.nix ];
+            installPhase = ''
+              mkdir -p $out/bin $out/lib/configs
+              cp -r ./configs $out/lib/
+              cp -r ./node_modules $out/lib/
+
+              wrapProgram ${pkgs.opencode}/bin/opencode \
+                --prefix PATH : ${pkgs.nix}/bin
+            '';
+          };
         };
-
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "opencode";
-          src = ./.;
-
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-          buildInputs = [];
-
-          installPhase = ''
-            mkdir -p $out/bin $out/lib/configs
-            cp -r ./configs $out/lib/
-            cp -r ${config.packages.node-modules}/node_modules $out/lib/
-
-            wrapProgram ${pkgs.opencode}/bin/opencode \
-              --prefix PATH : ${pkgs.nix}/bin
-          '';
-        };
-      };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
